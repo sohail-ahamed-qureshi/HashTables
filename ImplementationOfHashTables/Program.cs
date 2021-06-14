58b89861193855ced6aa71a89c957595a29c1b6e
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-06-14 20:19:48 +0530

    [sohail] Add: Welcome message

---
+++ ImplementationOfHashTables/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ImplementationOfHashTables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Implementation of Hashtables using LinkedList");
+        }
+    }
+}
